cb2e7617bad0a0fcfb25040bd5fcc765fe12461b
Manuel Jimena García <Manuel.jimena29@gmail.com>
2024-02-15 23:03:58 +0100

    first commit

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,31 @@
+import { SidebarContainer, SidebarTitle, ComponentList, ComponentListItem } from './StyledSidebar'
+
+const Sidebar = ({ onSelectComponent, selectedDeveloper, onSelectDeveloper, components, isVisible }) => {
+  const getIconClass = (name) => {
+    switch (name) {
+      case 'Buttons':
+        return 'fa-square'
+      case 'Spinners':
+        return 'fa-spinner'
+      case 'Toggles':
+        return 'fa-toggle-on'
+      default:
+        return 'fa-puzzle-piece'
+    }
+  }
+
+  return (
+    <SidebarContainer isVisible={isVisible}>
+      <SidebarTitle>Components</SidebarTitle>
+      <ComponentList>
+        {components.map(({ id, name }) => (
+          <ComponentListItem key={id} onClick={() => onSelectComponent(id)}>
+            <i className={`fa ${getIconClass(name)}`} aria-hidden="true"></i> {name}
+          </ComponentListItem>
+        ))}
+      </ComponentList>
+    </SidebarContainer>
+  )
+}
+
+export default Sidebar
---
+++ src/data/developers.js
@@ -0,0 +1,12 @@
+import manuelPhoto from '../assets/manuel.webp'
+
+const developers = [
+  {
+    id: 'MJG',
+    name: 'Manuel Jimena García',
+    photo: manuelPhoto,
+    components: ['Button_MJG', 'Spinner_MJG', 'Toggle_MJG']
+  }
+]
+
+export default developers
